6028c5885cbd6a4db69734c7ba9fa6fec3a6a8b1
Raymond <raymond.yiu@gmail.com>
2023-03-04 23:56:55 +0000

    create PairTest class

---
+++ src/test/java/org/example/PairTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairTest {
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void isMatch() {
+    }
+}
