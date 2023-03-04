8f1f2e1e26911e30d34423edc8a2e2e859ee2df2
Raymond <raymond.yiu@gmail.com>
2023-03-04 22:25:50 +0000

    create Straight class

---
+++ src/main/java/org/example/Straight.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Straight implements IMatch{
+    private String target;
+    private Rank rank = Rank.Straight;
+
+    Straight(){
+        target = new HashMap<>();
+        target = "";
+        for (CardValue cardValue : CardValue.values()){
+                target += cardValue.label();
+        }
+    }
+
+    public int getScore() {
+        return rank.score();
+    }
+    @Override
+    public boolean isMatch(ArrayList<Card> cards) {
+        String cardList="";
+        ArrayList<String> cardSymbolList = new ArrayList<>();
+        boolean match=false;
+        cards = Card.sort(cards);
+        for (Card card : cards){
+            cardList += card.getCardValue().label();
+        }
+        return (target.contains(cardList));
+    }
+}
