1346997fb23aa149db0b7c9adb5ad8f431343adb
Raymond <raymond.yiu@gmail.com>
2023-03-04 11:24:40 +0000

    update isMatch test for FourOfAKind class

--- src/test/java/org/example/FourOfAKindTest.java
+++ src/test/java/org/example/FourOfAKindTest.java
@@ -36,5 +36,30 @@ class FourOfAKindTest {
     }
     @Test
     void isMatch() {
+        FourOfAKind fourOfAKind = new FourOfAKind();
+        ArrayList<Card> cards = new ArrayList<Card>();
+        boolean expectedResult=true;
+        Card card;
+
+        List<String[]> csvTestCases = new ArrayList<>();
+        csvTestCases = readCsv("StraighOfAKindTestcases.csv");
+
+        for (String[] item : csvTestCases) {
+
+            expectedResult = Boolean.parseBoolean(item[0]);
+            String[] cardList = item[1].split(" ");
+            for (String cardStr : cardList) {
+
+                card = new Card(cardStr);
+                cards.add(card);
+            }
+
+            if (expectedResult) {
+                assertTrue(fourOfAKind.isMatch(cards));
+            } else {
+                assertFalse(fourOfAKind.isMatch(cards));
+            }
+            cards.clear();
+        }
     }
 }
