97ce2b7f06d9ece0c705422303982f702a9ecfd6
Raymond <raymond.yiu@gmail.com>
2023-03-04 17:04:44 +0000

    create FullHouse class

---
+++ src/main/java/org/example/FullHouse.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FullHouse implements IMatch{
+    private Map<String, String> targetThreeOfAKind;
+
+    private Map<String, String> targetPair;
+    private Rank rank = Rank.FourOfAKind;
+
+    FullHouse(){
+        targetThreeOfAKind = new HashMap<>();
+        targetPair = new HashMap<>();
+
+        for (CardValue cardValue : CardValue.values()) {
+            targetThreeOfAKind.put(cardValue.label(), cardValue.label() + cardValue.label() + cardValue.label());
+            targetPair.put(cardValue.label(), cardValue.label()+cardValue.label())
+        }
+    }
+    public int getScore() {
+        return rank.score;
+    }
+    public boolean isMatch(ArrayList<Card> cards) {
+        String cardList="";
+        boolean match=false;
+        for (Card card : cards){
+            cardList += card.getCardValue().label();
+        }
+
+        for (CardValue cardValue : CardValue.values()){
+            String targetStr = targetThreeOfAKind.get(cardValue.label());
+            if (cardList.contains(targetStr)) {
+                targetPair.remove(cardValue.label());
+                for (CardValue cardValue1 : CardValue.values()) {
+                    String targetStr1 = targetPair.get(cardValue1.label());
+                    match = cardList.contains(targetStr1);
+                    if (match) {
+                        break;
+                    }
+                }
+            }
+        }
+        return match;
+    }
+
+}
