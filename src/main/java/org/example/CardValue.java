4b604f885f09cf2ceb9d744d45273329bba915bb
Raymond <raymond.yiu@gmail.com>
2023-03-05 12:57:31 +0000

    update CardValue Enum class with new value defined

--- src/main/java/org/example/CardValue.java
+++ src/main/java/org/example/CardValue.java
@@ -4,18 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum CardValue {
-    2("2",2), 3("3",3), 4("4", 4), 5("5", 5), 6("6",6),
-    7("7", 7), 8("8",8 ), 9("9", 9), 10("T",10),
-    Jack("J",11), Queen("Q", 12), King("K",13), Ace("A",14);
+    Two("2","2",2), Three("3","3",3), Four("4","4", 4),
+    Five("5","5", 5), Six("6","6",6),Seven("7","7", 7),
+    Eight("8", "8",8 ), Nine("9", "9", 9), Ten("10","T",10),
+    Jack("Jack","J",11), Queen("Queen", "Q", 12), King("King","K",13),
+    Ace("Ace","A",14);
 
 
+    private final String value;
     private final String label;
     private final int score;
 
-    CardValue(String label, int score) {
+    CardValue(String value, String label, int score) {
+        this.value = value;
         this.label = label;
         this.score = score;
     }
+    public String value() {return value}
     public String label() { return label;}
     public int score() {return score;}
     private static final Map<String, CardValue> BY_LABEL = new HashMap<>();
